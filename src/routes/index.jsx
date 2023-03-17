758e74a6c872ba7f38afc03b54ac10345ad371d1
Jackson <jackson1712@hotmail.com>
2023-03-16 21:33:52 -0300

    configuration to enter according to user permissions, whether it is an administrator or not and if is authenticated

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
+import { useAuth } from "../hooks/auth";
 
 import { AppUsersRoutes } from "./appUser.routes";
 import { AppAdminRoutes } from "./appAdmin.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
     <BrowserRouter>
-      <AuthRoutes/>
+      { user ? ( user.isAdmin ? <AppAdminRoutes/> : <AppUsersRoutes/> ) : ( <AuthRoutes/>) }
     </BrowserRouter>
   );
 };
